72ffb06b46c1899eb15fffb547c4d8c55926e2d2
Erik Hallaraaker <erikhallaraaker@gmail.com>
2023-04-13 22:57:49 +0200

    run locally

--- components/Comments.js
+++ components/Comments.js
@@ -1,43 +1,34 @@
-import 'gitalk/dist/gitalk.css'
-import { useRouter } from 'next/router'
-import dynamic from 'next/dynamic'
-import cn from 'classnames'
-import { fetchCusdisLang } from '@/lib/cusdisLang'
-import { useConfig } from '@/lib/config'
+import "gitalk/dist/gitalk.css";
+import dynamic from "next/dynamic";
+import cn from "classnames";
+import { useConfig } from "@/lib/config";
 
 const GitalkComponent = dynamic(
   () => {
-    return import('gitalk/dist/gitalk-component')
+    return import("gitalk/dist/gitalk-component");
   },
   { ssr: false }
-)
+);
 const UtterancesComponent = dynamic(
   () => {
-    return import('@/components/Utterances')
+    return import("@/components/Utterances");
   },
   { ssr: false }
-)
-const CusdisComponent = dynamic(
-  () => {
-    return import('react-cusdis').then(m => m.ReactCusdis)
-  },
-  { ssr: false }
-)
+);
 
 const Comments = ({ frontMatter }) => {
-  const router = useRouter()
-  const BLOG = useConfig()
+  const BLOG = useConfig();
 
-  const fullWidth = frontMatter.fullWidth ?? false
+  const fullWidth = frontMatter.fullWidth ?? false;
 
   return (
     <div
       className={cn(
-        'px-4 font-medium text-gray-500 dark:text-gray-400 my-5',
-        fullWidth ? 'md:px-24' : 'mx-auto max-w-2xl',
+        "px-4 font-medium text-gray-500 dark:text-gray-400 my-5",
+        fullWidth ? "md:px-24" : "mx-auto max-w-2xl"
       )}
     >
-      {BLOG.comment && BLOG.comment.provider === 'gitalk' && (
+      {BLOG.comment && BLOG.comment.provider === "gitalk" && (
         <GitalkComponent
           options={{
             id: frontMatter.id,
@@ -47,28 +38,15 @@ const Comments = ({ frontMatter }) => {
             repo: BLOG.comment.gitalkConfig.repo,
             owner: BLOG.comment.gitalkConfig.owner,
             admin: BLOG.comment.gitalkConfig.admin,
-            distractionFreeMode: BLOG.comment.gitalkConfig.distractionFreeMode
+            distractionFreeMode: BLOG.comment.gitalkConfig.distractionFreeMode,
           }}
         />
       )}
-      {BLOG.comment && BLOG.comment.provider === 'utterances' && (
+      {BLOG.comment && BLOG.comment.provider === "utterances" && (
         <UtterancesComponent issueTerm={frontMatter.id} />
       )}
-      {BLOG.comment && BLOG.comment.provider === 'cusdis' && (
-        <CusdisComponent
-          lang={fetchCusdisLang(BLOG.lang)}
-          attrs={{
-            host: BLOG.comment.cusdisConfig.host,
-            appId: BLOG.comment.cusdisConfig.appId,
-            pageId: frontMatter.id,
-            pageTitle: frontMatter.title,
-            pageUrl: BLOG.link + router.asPath,
-            theme: BLOG.appearance
-          }}
-        />
-      )}
     </div>
-  )
-}
+  );
+};
 
-export default Comments
+export default Comments;
